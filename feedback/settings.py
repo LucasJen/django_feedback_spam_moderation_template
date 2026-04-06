19ac913602602f5aea0f3ea0d41b08c412ac3893
LucasJen <fake@fake.com>
2026-04-06 10:47:03 -0500

    start google task setup

--- feedback/settings.py
+++ feedback/settings.py
@@ -99,6 +99,7 @@ DATABASES = {
 
 if not RUNNING_AT_GCP: # on a local computer, connect to the Cloud SQL via the cloud SQL proxy tool
     DATABASES['default']['host'] = '127.0.0.1'
+    
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
